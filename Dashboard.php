21f33e474d6eadc578b696f182aba956a1537030
maruf96373 <157979463+maruf96373@users.noreply.github.com>
2024-03-04 16:46:13 +0600

    Lab 5.2

---
+++ Dashboard.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <title>Dashboard</title>
+    </head>
+<body>
+   
+<table height="400" width="500"  border="1" cellspacing="0">
+        <tr>
+            <td colspan=2 ><div align="left"><b>xCompany </b> </div> <div align="right">Logged in as<a style="color:rgb(0, 102, 255); " href="Bob">Bob</a>|
+            <a style="color:rgb(0, 102, 255);" href="Logout.php">Logout</a>
+</div>
+        </tr>
+        <tr  style="height:300;">
+            <td align="Left" style="width:200">
+        <b>Account</b><hr>
+ 
+                    <span align="left">
+                        <ul>
+                            <li><a href="page1.html">Dashboard</a></li>
+                            <li><a href="page1.html">view Profile</a></li>
+                            <li><a href="page1.html">Edit Profile</a></li>
+                            <li><a href="page1.html">Change Profile Picture</a></li>
+                            <li><a href="page1.html">Change Password</a></li>
+                            <li><a href="page1.html">Logout </a></li>
+                        </ul>
+</span>
+     
+                </td><td>Welcome BOB</td>
+                </tr>
+                <tr>
+                    <td colspan=2 align=center>Copyright@2017
+</td>
+</tr>
+                </table>
+               
+</body>
+</html>

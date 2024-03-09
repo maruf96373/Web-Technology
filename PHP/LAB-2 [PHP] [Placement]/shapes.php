8d978235740a622c6bd370585bad1e95b09fc684
maruf96373 <157979463+maruf96373@users.noreply.github.com>
2024-03-09 11:34:28 +0600

    Add files via upload

---
+++ PHP/LAB-2 [PHP] [Placement]/shapes.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+    <title>Shapes</title>
+</head>
+<body>
+    <table table border='1' cellspacing='0' width="300">
+        <tr>
+            <td>
+<?php
+for($i=0;$i<3;$i++){
+    for($j=0;$j<=$i;$j++){
+      print("*")  ;}
+      print("<br>");
+}
+?>
+</td>
+<td>
+    <?php
+for($i=3;$i>=1;$i--){
+    for($j=1;$j<=$i;$j++){
+      print($j)  ;}
+      print("<br>");
+} 
+?>
+</td>
+<td>
+    <?php
+$a='A';
+for($i=0;$i<3;$i++){
+    for($j=0;$j<=$i;$j++){
+      print($a++)  ;}
+      print("<br>");
+}
+?>
+</td>
+</tr>
+    </table>
+</body>
+</html>

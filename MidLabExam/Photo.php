f884052276fdb921720b3562d3c6a507467c90e0
maruf96373 <157979463+maruf96373@users.noreply.github.com>
2024-03-30 17:09:01 +0600

    Add files via upload

---
+++ MidLabExam/Photo.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <title>Album</title>
+    </head>
+<body>
+<table border="1" cellspacing="0" width="750">
+        <tr  style="height:200;">
+        <td align="Left" style="width:200">
+        <b>Dashboard</b><hr>
+ 
+                    <span align="left">
+                        <ul>
+                        <li><a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="topic3.php">Home</a></li>
+                            <li><a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="Profile.php">Profile</a></li>
+                            <li><a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="Project.php">Project</a></li>
+                            <li><a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="Contact.php">Contact</a></li>
+                            <li> <a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="Photo.php">Album</a></li>
+                              </ul>
+</span>
+     
+                </td><td align=top width="400"><span align=top>
+                    <fieldset width="200">
+                        <legend> Album</legend>
+                        <table>
+                            <tr>
+                                <td>
+                        <table>
+                        <tr>
+                            <td align =left><img src="cyber.jpg"width="200" height="250">  </td>      <td align =left>  <img src="web.jpg"width="200" height="250"></td>  
+                        </tr>
+                            <tr>
+                            <td align =left>  Cyber Security Event </td>      <td align =left>  Web Summit</td>  
+                        </tr>
+</table>
+</td>
+</tr>
+</table>                         
+                    </fieldset></td>
+                </tr>
+                <tr>
+                    <td colspan=2 align=center>Copyright © 2017
+</td>
+</tr>
+
+                </table>
+               
+</body>
+</html>
---
+++ MidLabExam/Project.php
@@ -0,0 +1,78 @@
+<html>
+    <head>
+        <title>Project</title>
+    </head>
+<body>
+<table border="1" cellspacing="0" width="750">
+        <tr  style="height:200;">
+        <td align="Left" style="width:100">
+        <b>Dashboard</b><hr>
+ 
+                    <span align="left">
+                        <ul>
+                        <li><a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="topic3.php">Home</a></li>
+                            <li><a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="Profile.php">Profile</a></li>
+                            <li><a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="Project.php">Project</a></li>
+                            <li><a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="Contact.php">Contact</a></li>
+                            <li> <a style="color:rgb(0, 102, 255);margin-top: 10px; padding:10px;" href="Photo.php">Album</a></li>
+                              </ul>
+</span>
+     
+                </td><td align=top width="400"><span align=top>
+                    <fieldset width="300">
+                        <legend> Project</legend>
+                        <table >
+                            <tr>
+                                <td>
+                                </table>
+        <table align="left" border="1" width="500" cellspacing=0>
+            <th>Feature</th>
+            <th>Specification Role</th>
+            <th>User story</th>
+           
+            <tr>
+                <td>Auth Feature</td>
+                <td>Guest, Admin, Staff</td>
+                <td>Registration, Login,  Forgot Password</td>
+               
+                
+            </tr>
+            <tr>
+                <td>Dashboard</td>
+                <td>Guest, Staff,Admin</td>
+                <td>Visit any page</th>
+                
+            </tr>
+            <tr>
+                <td>Guest Management</td>
+                <td>Admin</td>
+                <td>Update ID, Delete</td>
+                
+            </tr>
+            <tr>
+                <td>Profile </td>
+                <td>Guest</td>
+                <td>view profile,Edit profile, Change Password, Update Picture</td>
+                
+            </tr>
+            <tr>
+                <td>Calender </td>
+                <td>Admin,Staff</td>
+                <td>view Schedule, search ,add,edit and delete  Schedule</td>
+                
+            </tr>
+        
+            
+        </table>
+         
+                    </fieldset></td>
+                </tr>
+                <tr>
+                    <td colspan=2 align=center>Copyright © 2017
+</td>
+</tr>
+
+                </table>
+               
+</body>
+</html>

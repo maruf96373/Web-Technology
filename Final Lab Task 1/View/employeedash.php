a66fe0cde80debd1ee578d9e9d7b0cad496e87d9
maruf96373 <157979463+maruf96373@users.noreply.github.com>
2024-05-11 15:09:36 +0600

    Add files via upload

---
+++ Final Lab Task 1/View/employeedash.php
@@ -0,0 +1,28 @@
+<?php
+    if(!isset($_COOKIE['flag'])){
+        header('location: signin.php');
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Demo Dashboard</title>
+</head>
+<body>
+    <table width="100%" border="0">
+        <tr>
+            <td>
+                <a href="addProduct.php">Add Product</a> <br><br>
+                <a href="productlist.php">Product List</a><br><br>
+                <a href="../controller/logout.php">Logout</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p>Welcome <?php echo $_COOKIE['flag']; ?></p>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
+
